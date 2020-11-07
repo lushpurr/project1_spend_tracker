096486c0a2cd8ab3c838339aabed8b769f874c43
lushpurr <lushpurr@gmail.com>
2020-11-07 16:40:17 +0000

    test tag/merchant has id

--- app/tests/merchant_test.py
+++ app/tests/merchant_test.py
@@ -4,7 +4,10 @@ from models.merchant import Merchant
 class TestMerchant(unittest.TestCase):
 
     def setUp(self):
-        self.merchant1 = Merchant('BP')
+        self.merchant1 = Merchant('BP', 1)
 
     def test_merchant_has_name(self):
         self.assertEqual("BP", self.merchant1.name)
+
+    def test_merchant_has_id(self):
+        self.assertEqual(1, self.merchant1.id)
--- app/tests/tag_test.py
+++ app/tests/tag_test.py
@@ -3,7 +3,10 @@ from models.tag import Tag
 
 class TestTag(unittest.TestCase):
     def setUp(self):
-        self.tag1 = Tag('Fuel')
+        self.tag1 = Tag('Fuel', 1)
 
     def test_tag_has_category(self):
         self.assertEqual("Fuel", self.tag1.category)
+
+    def test_tag_has_id(self):
+        self.assertEqual(1, self.tag1.id)
