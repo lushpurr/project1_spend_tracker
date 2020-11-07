52c67132eba75bf85c6ee87e2c4214a22c88b13b
lushpurr <lushpurr@gmail.com>
2020-11-07 16:18:44 +0000

    added run_test.py and changed test folder to name tests

---
+++ app/run_tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+from tests.merchant_test import TestMerchant
+from tests.tag_test import TestTag
+from tests.transaction_test import TestTransaction
+
+if __name__ == '__main__':
+    unittest.main()
