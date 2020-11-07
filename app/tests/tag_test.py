092a6e4b081a482ea77e85e53c11d6c0b8321b32
lushpurr <lushpurr@gmail.com>
2020-11-07 16:36:28 +0000

    tag has category and merchant has name tests completed

--- app/tests/tag_test.py
+++ app/tests/tag_test.py
@@ -2,4 +2,8 @@ import unittest
 from models.tag import Tag
 
 class TestTag(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.tag1 = Tag('Fuel')
+
+    def test_tag_has_category(self):
+        self.assertEqual("Fuel", self.tag1.category)
