b84b47debce8b039073a0c7f28c11241894fa872
lushpurr <lushpurr@gmail.com>
2020-11-07 17:05:48 +0000

    set up console.py

---
+++ app/console.py
@@ -0,0 +1,23 @@
+import pdb
+from models.merchant import Merchant
+from models.tag import Tag
+from models.transaction import Transaction
+
+import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
+import repositories.transaction_repository as transaction_repository
+
+# transaction_repository.delete_all()
+# tag_repository.delete_all()
+# merchant_repository.delete_all()
+
+# merchant1 = Merchant('BP')
+# merchant_repository.save(merchant1)
+
+# tag1 = Tag('Fuel')
+# tag_repository.save(tag1)
+
+# transaction1 = Transaction(10.00, 'BP', 'Fuel')
+# transaction_repository.save(transaction1)
+
+pdb.set_trace()
