7b6d2e5459817a4de69ec6ee09771bdb28f48d4a
lushpurr <lushpurr@gmail.com>
2020-11-07 17:42:25 +0000

    transaction_repository coded

--- app/repositories/transaction_repository.py
+++ app/repositories/transaction_repository.py
@@ -1,2 +1,38 @@
 from db.run_sql import run_sql
 from models.transaction import Transaction
+
+
+import repositories.tag_repository as tag_repository
+import repositories.merchant_repository as merchant_repository
+
+def save(transaction):
+    sql = "INSERT INTO transactions ( amount, merchant_id, tag_id ) VALUES ( %s, %s, %s ) RETURNING id"
+    values = [transaction.id, transaction.merchant.id, transaction.tag.id]
+    results = run_sql( sql, values )
+    transaction.id = results[0]['id']
+    return transaction
+
+
+def select_all():
+    transactions = []
+
+    sql = "SELECT * FROM transactions"
+    results = run_sql(sql)
+
+    for row in results:
+        
+        merchant = merchant_repository.select(row['merchant_id'])
+        tag = tag_repository.select(row['tag_id'])
+        transaction = Transaction(row['amount'], merchant, tag, row['id'])
+        transactions.append(transaction)
+    return transactions
+
+
+def delete_all():
+    sql = "DELETE FROM transactions"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM transactions WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
