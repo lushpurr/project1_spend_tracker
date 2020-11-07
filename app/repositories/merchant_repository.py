080ef690f17f06a1e9d0041d3d5da177838b19aa
lushpurr <lushpurr@gmail.com>
2020-11-07 17:16:14 +0000

    merchant_repository coded

--- app/repositories/merchant_repository.py
+++ app/repositories/merchant_repository.py
@@ -1,2 +1,37 @@
 from db.run_sql import run_sql
+from models.merchant import Merchant
 
+def save(merchant):
+    sql = "INSERT INTO merchants(name) VALUES ( %s ) RETURNING id"
+    values = [merchant.name]
+    results = run_sql( sql, values )
+    merchant.id = results[0]['id']
+    return merchant
+
+
+def select_all():
+    merchants = []
+
+    sql = "SELECT * FROM merchants"
+    results = run_sql(sql)
+
+    for row in results:
+        merchant = Merchant(row['name'], row['id'])
+        merchants.append(merchant)
+    return merchants
+
+
+def select(id):
+    merchant = None
+    sql = "SELECT * FROM merchants WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        merchant = Merchant(result['name'], result['id'] )
+    return merchant
+
+
+def delete_all():
+    sql = "DELETE FROM merchants"
+    run_sql(sql)
