5f9ecaf8944064bc1407cbd59da49f36a76dc159
lushpurr <lushpurr@gmail.com>
2020-11-08 13:02:30 +0000

    update  merchant_controller and tag_controller

--- app/controllers/merchant_controller.py
+++ app/controllers/merchant_controller.py
@@ -5,13 +5,13 @@ import repositories.merchant_repository as merchant_repository
 
 merchants_blueprint = Blueprint("merchants", __name__)
 
-@locations_blueprint.route("/merchants")
+@merchants_blueprint.route("/merchants")
 def merchants():
     merchants = merchant_repository.select_all() # NEW
     return render_template("merchants/index.html", merchants = merchants)
 
-@merchants_blueprint.route("/merchants/<id>")
-def show(id):
-    merchant = merchant_repository.select(id)
-    tags = merchant_repository.users(merchant)
-    return render_template("merchants/show.html", merchant=merchant, tags=tags)
+# @merchants_blueprint.route("/merchants/<id>")
+# def show(id):
+#     merchant = merchant_repository.select(id)
+#     tags = merchant_repository.tags(merchant)
+#     return render_template("merchants/show.html", merchant=merchant, tags=tags)
---
+++ app/controllers/tag_controller.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.tag import Tag
+import repositories.merchant_repository as merchant_repository
+
+tags_blueprint = Blueprint("tags", __name__)
+
+@tags_blueprint.route("/tags")
+def tags():
+    tags = tag_repository.select_all() # NEW
+    return render_template("tags/index.html", tags = tags)
+
+# @tags_blueprint.route("/tags/<id>")
+# def show(id):
+#     tag = tag_repository.select(id)
+#     merchants = tag_repository.users(tag)
+#     return render_template("tags/show.html", tag=tag, merchants=merchants)
