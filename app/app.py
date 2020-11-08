c1b7a1e28f3af2121fd99c2256ffcce7990439fb
lushpurr <lushpurr@gmail.com>
2020-11-08 12:38:45 +0000

    created app.py

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+from controllers.transaction_controller import transactions_blueprint
+from controllers.merchant_controller import merchants_blueprint
+from controllers.tag_controller import tags_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(transactions_blueprint)
+app.register_blueprint(merchants_blueprint)
+app.register_blueprint(tags_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
