88cc89720f6eb128e514c2dc1efd9a34931ba253
lushpurr <lushpurr@gmail.com>
2020-11-08 13:24:28 +0000

    edited tag_controller

--- app/controllers/tag_controller.py
+++ app/controllers/tag_controller.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.tag import Tag
-import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
 
 tags_blueprint = Blueprint("tags", __name__)
 
