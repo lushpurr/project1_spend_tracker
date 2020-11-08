9aca1259f9c41a1900cfd7c41693664780ff7b53
lushpurr <lushpurr@gmail.com>
2020-11-08 17:08:28 +0000

    added create tag route

--- app/controllers/tag_controller.py
+++ app/controllers/tag_controller.py
@@ -5,13 +5,49 @@ import repositories.tag_repository as tag_repository
 
 tags_blueprint = Blueprint("tags", __name__)
 
+#index
 @tags_blueprint.route("/tags")
 def tags():
-    tags = tag_repository.select_all() # NEW
+    tags = tag_repository.select_all() 
     return render_template("tags/index.html", tags = tags)
 
-# @tags_blueprint.route("/tags/<id>")
-# def show(id):
-#     tag = tag_repository.select(id)
-#     merchants = tag_repository.users(tag)
-#     return render_template("tags/show.html", tag=tag, merchants=merchants)
+
+
+# NEW
+
+@tags_blueprint.route("/tags/new")
+def new_tag():
+    return render_template(tags/new.html)
+
+
+
+# CREATE
+@tags_blueprint.route("/tags", methods=["POST"])
+def create_tag():
+    category = request.form["category"]
+    new_tag = Tag(category)
+    tag_repository.save(new_tag)
+    return redirect("/tags")
+
+
+# # EDIT
+# @humans_blueprint.route("/humans/<id>/edit")
+# def edit_human(id):
+#     human = human_repository.select(id)
+#     return render_template('humans/edit.html', human=human)
+
+
+# # UPDATE
+# @humans_blueprint.route("/humans/<id>", methods=["POST"])
+# def update_human(id):
+#     name = request.form["name"]
+#     human = Human(name, id)
+#     human_repository.update(human)
+#     return redirect("/humans")
+
+
+# # DELETE
+# @humans_blueprint.route("/humans/<id>/delete", methods=["POST"])
+# def delete_human(id):
+#     human_repository.delete(id)
+#     return redirect("/humans")
