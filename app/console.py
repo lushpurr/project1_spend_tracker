5376da2f6e057db3ca28c296690ac2dd3ad963d3
lushpurr <lushpurr@gmail.com>
2020-11-09 17:18:31 +0000

    active column test data and updated merchant_repo with new values

--- app/console.py
+++ app/console.py
@@ -11,16 +11,16 @@ transaction_repository.delete_all()
 tag_repository.delete_all()
 merchant_repository.delete_all()
 
-merchant1 = Merchant('BP', 'Glasgow')
+merchant1 = Merchant('BP', True)
 merchant_repository.save(merchant1)
 
-merchant2 = Merchant('Wetherspoons', "Carlisle")
+merchant2 = Merchant('Wetherspoons', True)
 merchant_repository.save(merchant2)
 
-tag1 = Tag('Fuel')
+tag1 = Tag('Fuel', True)
 tag_repository.save(tag1)
 
-tag2 = Tag('Food')
+tag2 = Tag('Food', True)
 tag_repository.save(tag2)
 
 transaction1 = Transaction(10.00, merchant1, tag1)
--- app/repositories/merchant_repository.py
+++ app/repositories/merchant_repository.py
@@ -2,8 +2,8 @@ from db.run_sql import run_sql
 from models.merchant import Merchant
 
 def save(merchant):
-    sql = "INSERT INTO merchants( name ) VALUES ( %s ) RETURNING id"
-    values = [merchant.name,]
+    sql = "INSERT INTO merchants( name, active ) VALUES ( %s, %s ) RETURNING id"
+    values = [merchant.name, merchant.active]
     results = run_sql( sql, values )
     merchant.id = results[0]['id']
     return merchant
@@ -16,7 +16,7 @@ def select_all():
     results = run_sql(sql)
 
     for row in results:
-        merchant = Merchant(row['name'], row['id'])
+        merchant = Merchant(row['name'], row['active'], row['id'])
         merchants.append(merchant)
     return merchants
 
@@ -28,7 +28,7 @@ def select(id):
     result = run_sql(sql, values)[0]
 
     if result is not None:
-        merchant = Merchant(result['name'], result['id'] )
+        merchant = Merchant(result['name'], result['active'], result['id'] )
     return merchant
 
 
@@ -42,6 +42,6 @@ def delete(id):
     run_sql(sql, values)
 
 def update(merchant):
-    sql = "UPDATE merchants SET name = %s WHERE id = %s"
-    values = [merchant.name, merchant.id]
+    sql = "UPDATE merchants SET ( name, active ) = ( %s, %s ) WHERE id = %s"
+    values = [merchant.name, merchant.active, merchant.id]
     run_sql(sql, values)
