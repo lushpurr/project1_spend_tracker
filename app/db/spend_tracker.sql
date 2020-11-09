1d5e424428dc6a5d4c018d8445f9b24fd546b5fd
lushpurr <lushpurr@gmail.com>
2020-11-09 17:04:26 +0000

    added active row to tags and merchants tables

--- app/db/spend_tracker.sql
+++ app/db/spend_tracker.sql
@@ -4,12 +4,14 @@ DROP TABLE tags;
 
 CREATE TABLE tags (
     id SERIAL PRIMARY KEY, 
-    category VARCHAR(255)
+    category VARCHAR(255),
+    active BOOLEAN
 );
 
 CREATE TABLE merchants (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255), 
+    active BOOLEAN
 );
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY, 
