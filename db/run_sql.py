bbb98a542050de2cf68607b17106004fd86ed007
lushpurr <lushpurr@gmail.com>
2021-01-22 12:11:19 +0000

    updated run_sql to connect to heroku db

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,12 +1,17 @@
 import psycopg2
 import psycopg2.extras as ext
+import os
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 
 def run_sql(sql, values = None):
     conn = None
     results = []
 
     try:
-        conn=psycopg2.connect("dbname='spend_tracker'")
+        # conn=psycopg2.connect("dbname='spend_tracker'")
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
